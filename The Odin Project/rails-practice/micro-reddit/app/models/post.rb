f3b6c334255ea3d265620eab018c8bfa8337d06f
Sébastien Marleau <59572972+shoebe@users.noreply.github.com>
2022-02-26 00:09:29 -0500

    Remove to stop getting alers

--- The Odin Project/rails-practice/micro-reddit/app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ApplicationRecord
-  validates :user_id, presence: true
-  validates :url, presence: true
-  belongs_to :user
-  has_many :comments
-end
--- The Odin Project/rails-practice/re-former/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  validates :username, presence: true,uniqueness: true, length: {in: 1..30}
-  validates :password, presence: true, length: {in: 6..30}
-  validates :email, presence: true, format: { with: /@/, message: "Is an email"}
-end
